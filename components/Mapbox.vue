9fd3494f29329de519135e2f6acc75883b062cec
Charlie <charlie15001@gmail.com>
2024-07-09 13:13:07 +0800

    add route between current and clicked location

--- components/Mapbox.vue
+++ components/Mapbox.vue
@@ -13,6 +13,55 @@ import bikeData from '~/assets/data/youbike_immediate.json'
 import transformToGeoJSON from '~/utils/transformToGeoJSON'
 import { getUserLocation } from '~/utils'
 
+const mapboxAccessToken = 'pk.eyJ1IjoiY2hhcmxpZTE1MDAxIiwiYSI6ImNseWI2cjY5dTB2Ym0yanF3dWFhM3lzaGgifQ.H-XYHkcUG289Yj0cndv8TA'
+
+// create a function to make a directions request
+async function getRoute(viewer, start, end) {
+  // make a directions request using cycling profile
+  // an arbitrary start will always be the same
+  // only the end or destination will change
+  const query = await fetch(
+    `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxAccessToken}`,
+    { method: 'GET' }
+  );
+  const json = await query.json();
+  const data = json.routes[0];
+  const route = data.geometry.coordinates;
+  const geojson = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'LineString',
+      coordinates: route
+    }
+  };
+  // if the route already exists on the map, we'll reset it using setData
+  if (viewer.getSource('route')) {
+    viewer.getSource('route').setData(geojson);
+  }
+  // otherwise, we'll make a new request
+  else {
+    viewer.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: geojson
+      },
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#3887be',
+        'line-width': 5,
+        'line-opacity': 0.75
+      }
+    });
+  }
+  // add turn instructions here at the end
+}
+
 const { cameraState } = useCameraState()
 
 const geoJSONData = transformToGeoJSON(bikeData)
@@ -58,8 +107,9 @@ const initializeCesium = (lng, lat) => {
 }
 
 const initializeMapbox = (lng, lat) => {
+  const start_coords = [lng, lat];
   const map = new mapboxgl.Map({
-    accessToken: 'pk.eyJ1IjoiY2hhcmxpZTE1MDAxIiwiYSI6ImNseWI2cjY5dTB2Ym0yanF3dWFhM3lzaGgifQ.H-XYHkcUG289Yj0cndv8TA',
+    accessToken: mapboxAccessToken,
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v12',
     center: [lng, lat],
@@ -146,8 +196,86 @@ const initializeMapbox = (lng, lat) => {
             }
           })
         }
-    );
+    )
+    // make an initial directions request that
+    // starts and ends at the same location
+    getRoute(map, start_coords, start_coords);
+
+    // Add starting point to the map
+    map.addLayer({
+      id: 'point',
+      type: 'circle',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'Point',
+                coordinates: start_coords
+              }
+            }
+          ]
+        }
+      },
+      paint: {
+        'circle-radius': 10,
+        'circle-color': '#3887be'
+      }
+    });
+    // this is where the code from the next step will go
   })
+
+  map.on('click', (event) => {
+    const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
+    const end = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'Point',
+            coordinates: coords
+          }
+        }
+      ]
+    };
+    if (map.getLayer('end')) {
+      map.getSource('end').setData(end);
+    } else {
+      map.addLayer({
+        id: 'end',
+        type: 'circle',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: [
+              {
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                  type: 'Point',
+                  coordinates: coords
+                }
+              }
+            ]
+          }
+        },
+        paint: {
+          'circle-radius': 10,
+          'circle-color': '#f30'
+        }
+      });
+    }
+    getRoute(map, start_coords, coords);
+    console.log('Start point: ', start_coords)
+    console.log('End point: ', coords)
+  });
 
   var popup = new mapboxgl.Popup()
     .setHTML('<h1>Current Position</h1>')
